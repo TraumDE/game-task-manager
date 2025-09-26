7959a5896c1161397b11b3a35c8d30dad21c77cb
TraumDE <eugene301workmaik@gmail.com>
2025-09-26 15:49:06 +0300

    return postcss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CyrcleProgressBar from './components/CyrcleProgressBar.vue'
+</script>
 
 <template>
-  <main class="container">
+  <main>
+    <CyrcleProgressBar />
     <h1>Hello world!</h1>
     <p>dsasadas</p>
   </main>
